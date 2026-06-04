ca092b5d245d255b6abcd5a79f98036c1e0f0747
wyrtwala <89209191+wyrtwala@users.noreply.github.com>
2026-06-03 21:27:48 -0700

    Update tcp.h

--- tcp.h
+++ tcp.h
@@ -10,7 +10,7 @@
 #include <sys/types.h>
 #include <arpa/inet.h>
 
-int tcp_send_ipv4(char* ip, int port, char* buffer, uint_64 buffer_size){}
+int tcp_send_ipv4(char* ip, int port, char* message, char* buffer, uint_64 buffer_size){}
 
   if ((int socket = socket(AF_INET, SOCK_STREAM, 0)) < 0){
     fprintf(stderr, "TCP_SEND_IPV4: Failed to open socket\n");
@@ -21,19 +21,23 @@ int tcp_send_ipv4(char* ip, int port, char* buffer, uint_64 buffer_size){}
   address.sin_family = AF_INET;
   address.sin_port = htons(port);
 
-  if (inet_aton(ip, (struct in_addr *) &(address.sin_addr.s_addr)) != 0){
+  if (inet_aton(ip, (struct in_addr *) &(address.sin_addr.s_addr)) < 0){
     fprintf(stderr, "TCP_SEND_IPV4: Incorrect IP address\n");
     return -1;
   }
-  if (connect(socket, (struct sockaddr *) &address, sizeof(address)) != 0){
-    fprintf(stderr, "TCP_SEND_IPV4: Failed to connect to %s:%d\n", ip, port);
+  if (connect(socket, (struct sockaddr *) &address, sizeof(address)) < 0){
+    fprintf(stderr, "TCP_SEND_IPV4: Failed to connect\n");
     return -1;
   }
-  uint64_t message_size = buffersize - 1;
-  if (recv(socket, buffer, message_size, 0) != 0){
-    fprintf(stderr, "TCP_SEND_IPV4: Failed to recieve from %s:%d\n", ip, port);
+  if(send(socket, message, strlen(message), 0) < 0){
+    fprintf(stderr, "TCP_SEND_IPV4: Failed to send\n");
+    return -1;
   }
-  if (close(socket) != 0){
+  uint64_t return_message_size = buffersize - 1;
+  if (recv(socket, buffer, return_message_size, 0) < 0){
+    fprintf(stderr, "TCP_SEND_IPV4: Failed to recieve\n");
+  }
+  if (close(socket) < 0){
     fprintf(stderr, "TCP_SEND_IPV4: Failed to close socket\n");
     return -1;
   }
@@ -41,7 +45,7 @@ int tcp_send_ipv4(char* ip, int port, char* buffer, uint_64 buffer_size){}
   return 0;
 }
 
-int tcp_simple_server_ipv4(char* ip, int port, char* message, uint64_t message_size, uint64_t listen_count){
+int tcp_simple_server_ipv4(char* ip, int port, char* message, uint64_t message_size, char* client_message, uint64_t client_message_size, uint64_t listen_count){
   
     if ((int server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0){
       fprintf(stderr, "TCP_SIMPLE_SERVER_IPV4: Failed to open socket\n");
@@ -53,11 +57,11 @@ int tcp_simple_server_ipv4(char* ip, int port, char* message, uint64_t message_s
     address.sin_port = htons(port);
     address.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(server_socket, (struct sockaddr *) &address, sizeof(address)) != 0){
-      fprintf(stderr, "TCP_SIMPLE_SERVER_IPV4: Failed to bind at port %d\n", port);
+    if (bind(server_socket, (struct sockaddr *) &address, sizeof(address)) < 0){
+      fprintf(stderr, "TCP_SIMPLE_SERVER_IPV4: Failed to bind\n");
       return -1;
     }
-    if (listen(server_socket, listen_count) != 0){
+    if (listen(server_socket, listen_count) < 0){
       fprintf(stderr, "TCP_SIMPLE_SERVER_IPV4: Failed listener\n");
       return -1;
     }
@@ -65,11 +69,19 @@ int tcp_simple_server_ipv4(char* ip, int port, char* message, uint64_t message_s
       fprintf(stderr, "TCP_SIMPLE_SERVER_IPV4: Failed accept\n");
       return -1;
     }
-    if (send(client_socket, message, message_size, 0) != 0){
+    if (recv(client_socket, client_message, sizeof(client_message), 0) < 0){
+      fprintf(stderr, "TCP_SIMPLE_SERVER_IPV4: Failed to recieve\n");
+      return -1;
+    }
+    if (sizeof(client_message) > client_message_size){
+      fprintf(stderr, "TCP_SIMPLE_SERVER_IPV4: Client message too long\n");
+      return -1;
+    }
+    if (send(client_socket, message, message_size, 0) < 0){
       fprintf(stderr, "TCP_SIMPLE_SERVER_IPV4: Failed to send message\n");
       return -1;
     }
-    if(close(client_socket) != 0){
+    if(close(client_socket) < 0){
       fprintf(stderr, "TCP_SIMPLE_SERVER_IPV4: Failed to close socket\n");
       return -1;
     }
